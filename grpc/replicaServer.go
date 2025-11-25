2940d577ed103c88e71061039ed76b93d28e7bae
Elisa Esgici <elie@itu.dk>
2025-11-25 18:03:34 +0100

    Update replicaServer.go

--- grpc/replicaServer.go
+++ grpc/replicaServer.go
@@ -1,28 +1,38 @@
 package grpc
 
-type replicaServer struct {
+import (
+	proto "Mandatory-Activity-5/grpc"
+	"context"
+)
+
+type ReplicaServer struct {
+	proto.UnimplementedReplicaServer
+
+	HighestBid  int32
+	AuctionOver bool
+	Winner      string
 }
 
-func newReplicaServer() replicaServer {
-	return replicaServer{}
+func NewReplicaServer() *ReplicaServer {
+	return &ReplicaServer{
+		HighestBid:  0,
+		AuctionOver: false,
+		Winner:      "",
+	}
 }
 
-func replicaBid() {
-
+func (r *ReplicaServer) ReplicateBid(ctx context.Context, in *proto.ReplicaBidState) (*proto.ReplicaAck, error) {
+	r.HighestBid = in.HighestBid
+	r.AuctionOver = in.AuctionOver
+	r.Winner = in.Winner
+	return &proto.ReplicaAck{Ok: true}, nil
 }
 
-func syncState() {
-
-}
-
-func broadcastState() {
-
-}
-
-func heartbeat() {
-
-}
-
-func handleFaliure() {
-
+func (r *ReplicaServer) SyncState(ctx context.Context, in *proto.SyncRequest) (*proto.SyncReply, error) {
+	reply := &proto.SyncReply{
+		HighestBid:  r.HighestBid,
+		AuctionOver: r.AuctionOver,
+		Winner:      r.Winner,
+	}
+	return reply, nil
 }
